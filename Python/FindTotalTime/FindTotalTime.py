4974f19400a38edea0cd0b0520a36f55c6236a38
patilhiteshb <patilhiteshb@gmail.com>
2023-04-15 23:14:25 +0530

    Update -FindTotalTime

---
+++ Python/FindTotalTime/FindTotalTime.py
@@ -0,0 +1,57 @@
+from typing import List
+
+
+class Solution:
+    def totalTime(self, n : int, arr : List[int], time : List[int]) -> int:
+        # code here
+        mp = {}
+        ans = 0
+        
+        for i in arr:
+            if i not in mp:
+                mp[i] = 0
+                ans += 1
+            else:
+                ans += time[i-1]
+                
+        return ans-1
+        
+
+
+        
+
+
+
+#{ 
+ # Driver Code Starts
+class IntArray:
+    def __init__(self) -> None:
+        pass
+    def Input(self,n):
+        arr=[int(i) for i in input().strip().split()]#array input
+        return arr
+    def Print(self,arr):
+        for i in arr:
+            print(i,end=" ")
+        print()
+
+
+if __name__=="__main__":
+    t = int(input())
+    for _ in range(t):
+        
+        n = int(input())
+        
+        
+        arr=IntArray().Input(n)
+        
+        
+        time=IntArray().Input(n)
+        
+        obj = Solution()
+        res = obj.totalTime(n, arr, time)
+        
+        print(res)
+        
+
+# } Driver Code Ends
