571cb6cefe2de3baf03486d702d7cc739b4aab99
patilhiteshb <patilhiteshb@gmail.com>
2023-12-18 23:03:05 +0530

    Update -GameOfXOR

---
+++ Python/GameOfXOR/GameOfXOR.py
@@ -0,0 +1,23 @@
+#User function Template for python3
+
+class Solution:
+    def gameOfXor(self, N , A):
+        ans=0
+        for i,v in enumerate(A):
+            if (i+1)*(N-i)%2==1:
+                ans^=v
+        return ans
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        N=int(input())
+        A=list(map(int,input().split()))
+        
+        ob = Solution()
+        print(ob.gameOfXor(N,A))
+# } Driver Code Ends
